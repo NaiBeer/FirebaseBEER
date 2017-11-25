baee4365d9262f60665d00b12479858618f65ff2
NaiBeerAD <atitheppremphu@gmail.com>
2017-11-25 14:28:04 +0700

    Replace Fragment Success

--- app/src/main/java/com/livessolutions/firebasebeer/fragment/MainFragment.java
+++ app/src/main/java/com/livessolutions/firebasebeer/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.livessolutions.firebasebeer.R;
 
@@ -14,6 +15,33 @@ import com.livessolutions.firebasebeer.R;
  */
 
 public class MainFragment extends Fragment {
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Register Controller
+
+        registerController();
+
+
+    } // Main Method onActivityCreated
+
+    private void registerController() {
+        TextView textView = getView().findViewById(R.id.txtNewRegister);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction().replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null).commit();
+
+
+
+            } // OnClick
+        });
+    }
 
     @Nullable
     @Override
